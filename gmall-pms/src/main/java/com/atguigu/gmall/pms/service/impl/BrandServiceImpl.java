c776e3707acd1264abb9f5557f0473cef1e7b982
chenhaiyan <chenhaiyan@gatherlife.cn>
2019-12-25 11:14:48 +0800

    xx

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/BrandServiceImpl.java
@@ -0,0 +1,22 @@
+package com.atguigu.gmall.pms.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.pms.entity.Brand;
+import com.atguigu.gmall.pms.mapper.BrandMapper;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * 品牌表 服务实现类
+ * </p>
+ *
+ * @author Chy
+ * @since 2019-12-23
+ */
+@Component
+@Service
+public class BrandServiceImpl extends ServiceImpl<BrandMapper, Brand> implements BrandService {
+
+}
---
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -0,0 +1,33 @@
+package com.atguigu.gmall.pms;
+
+import com.atguigu.gmall.pms.entity.Brand;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.atguigu.gmall.pms.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class GmallPmsApplicationTests {
+
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    BrandService brandService;
+
+    @Test
+    void contextLoads() {
+        /*Product byId = productService.getById(1);
+        System.out.println(byId);*/
+        /*Brand brand = new Brand();
+        brand.setName("哈哈哈");
+        brandService.save(brand);
+        System.out.println("保存成功。。。。。");*/
+        Brand byId = brandService.getById(53);
+        System.out.println(byId.getName());
+    }
+
+}
